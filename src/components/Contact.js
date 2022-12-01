2242fb47be6e1bc564ba43b3a81e12efd7307960
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-12-01 14:11:29 -0500

    Add Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+
+function Contact () {
+    return (
+        
+    <section id="Contact-Me" class="Contact-Me">
+            
+        <h2>Contact Me</h2>
+        <p id="contact-links"> 555-555-5555 </p>
+        <p><a id="contact-links" href = "mailto:bilalfarrukh2000@gmail.com" target="_blank">bilalfarrukh2000@gmail.com</a></p>
+        <p><a id="contact-links" href = "https://github.com/bilalfarrukh2000" target="_blank">GitHub</a></p>
+    </section>
+    )
+}
+
+export default Contact;

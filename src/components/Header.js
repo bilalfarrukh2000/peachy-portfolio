07a54ca228ad7b3ad2c689895c2baed37a7f6ce8
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-12-01 13:41:08 -0500

    Add Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <nav class="header">
+        <h1>Bilal Farrukh</h1>
+        <nav><Navigation /></nav>
+        </nav>
+    )
+}
+
+export default Header;
